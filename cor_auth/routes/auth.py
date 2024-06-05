300e4ef63f7000e880f0da51537d3de8c9a0fee6
ZhovtenkoA <zhowtenko.oleksiy@gmail.com>
2024-06-05 15:00:52 +0300

    forgot pass upd

--- cor_auth/routes/auth.py
+++ cor_auth/routes/auth.py
@@ -92,7 +92,7 @@ async def login(
     refresh_token = await auth_service.create_refresh_token(data={"id": user.id})
     await repository_users.update_token(user, refresh_token, db)
     redirect_url = f"https://cor-platform.azurewebsites.net/?access_token={access_token}"
-    #return RedirectResponse(redirect_url)                                                      #Редирект на платформу с передачей токена 
+    # return RedirectResponse(redirect_url)                                                      #Редирект на платформу с передачей токена 
     return {
         "access_token": access_token,
         "refresh_token": refresh_token,
