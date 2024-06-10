52a94c917bf1c47d3ca654bc72be6a1ffb8ac7fa
ZhovtenkoA <zhowtenko.oleksiy@gmail.com>
2024-06-10 12:37:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -99,4 +99,4 @@ app.include_router(record.router, prefix="/api")
 
 
 if __name__ == "__main__":
-    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app="main:app", host="0.0.0.0", reload=True)
